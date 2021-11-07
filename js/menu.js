af730959646ab1a3f44d82326b154809b64370da
Sinkevich ILona <63476214+ifSinkevich@users.noreply.github.com>
2021-11-07 19:48:18 +0300

    Динамическая отрисовка карточек товара

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,63 @@
-const restaurant = 'tanuki';
+const cardsMenu = document.querySelector('.cards-menu');
+
+const changeTitle = (restaurant) => {
+
+  const restaurantTitle = document.querySelector('.restaurant-title'),
+  restaurantRating = document.querySelector('.rating'),
+  restaurantPrice = document.querySelector('.price'),
+  restaurantCategory = document.querySelector('.category');
+
+  restaurantTitle.textContent = restaurant.name;
+  restaurantRating.textContent = restaurant.stars;
+  restaurantPrice.textContent = 'От '+ restaurant.price + ' ₽';
+  restaurantCategory.textContent = restaurant.kitchen;
+};
 
 const renderItems = (data) => {
-  data.forEach(element => {
-    console.log(element);
+  data.forEach(({description, id, image, name, price}) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+      <img src="${image}" alt="${name}" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">${description}</div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price} ₽</strong>
+        </div>
+      </div>
+    `;
+
+    cardsMenu.append(card);
   });  
 };
 
-fetch(`./db/${restaurant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+if (localStorage.getItem('restaurant')) {
+
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'));
+
+    changeTitle(restaurant);
+
+    fetch(`./db/${restaurant.products}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  } else {
+    window.location.href = '/';
+  }
+
+
